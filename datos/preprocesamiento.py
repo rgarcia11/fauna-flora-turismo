c90cd01a82e9d694c603a3714ba6e0e1f9391845
rgarcia11 <r.garcia11@uniandes.edu.co>
2018-03-06 10:55:23 -0500

    Comenzando a arreglar lo de la fuente 1

--- datos/preprocesamiento.py
+++ datos/preprocesamiento.py
@@ -17,9 +17,13 @@ print('cargado archivo fuente 2. Filas: %d ' %len(f2))
 #f1
 
 print(f1.columns)
+del f1['gbifid']
+del f1['occurrenceid']
 del f1['basisofrecord']
+del f1['publishingorgkey']
 del f1['coordinateuncertaintyinmeters']
 del f1['coordinateprecision']
+del f1['infraspecificepithet']
 del f1['datasetkey']
 del f1['day']
 del f1['depth']
@@ -32,9 +36,23 @@ del f1['mediatype']
 del f1['recordnumber']
 del f1['rightsholder']
 del f1['typestatus']
-
+del f1['identifiedby']
+del f1['recordedby']
+del f1['issue']
+del f1['lastinterpreted']
 print('borradas columnas de la fuente 1')
-f1.to_csv(output1)
+
+#borrar filas que no sean de Colombia
+print('longitud archivo 1: %d'%len(f1))
+indices_borrar = f1['countrycode'] != 'CO'
+f1 = f1.drop(f1.index[indices_borrar])
+indices_borrar = f1['year'] < 1995
+f1 = f1.drop(f1.index[indices_borrar])
+print('longitud archivo 1: %d'%len(f1))
+
+
+
+
 #f2
 #Quitar duplicados
 f2.drop_duplicates(subset=None, inplace=True)
@@ -55,7 +73,7 @@ for filaDT in range(0,len(f2['direccion_territorial'])):
         datico = 'GUACHAROS'
     elif datico == 'ISLA DE LA COROTA':
         datico = 'COROTA'
-    elif datico == 'ISLA DE SALAMANCA':
+    elif datico == 'ISLA DE SALAMANCA' or datico == 'SLA DE SALAMANCA':
         datico = 'SALAMANCA'
     elif datico == 'EL COCUY':
         datico = 'COCUY'
@@ -65,6 +83,8 @@ for filaDT in range(0,len(f2['direccion_territorial'])):
         datico = 'GUANENTA FONCE'
     elif datico == 'NEVADOS':
         datico = 'LOS NEVADOS'
+    elif datico == 'CORALES DEL ROSARIO':
+        datico = 'CORALES ROSARIO'
 
     f2.iloc[filaDT, columna_area_protegida] = datico
     SinDireccionTerritorial = f2.iloc[filaDT, columna_direccion_territorial]
@@ -74,7 +94,6 @@ for filaDT in range(0,len(f2['direccion_territorial'])):
 print('arreglado formato y nombres')
 #Aqui se copia el f2 en f3 para poder manejar por aparte la categorizacion
 f3 = f2.copy()
-print(f3.head())
 #categorizar las columnas que estan segmentadas por tipo de visitante
 #0 -> 0
 #1-100 -> 1
@@ -99,6 +118,9 @@ def categorizarSegmentados(ingresos):
     else:
         ingresos = 0
     return ingresos
+
+#def mesANumero(mes):
+    #if mes == 'enero'
 
 for columnaDT in range(inicio_columnas_segmentadas, fin_columnas_segmentadas):
     for filaDT in range(0, len(f2['adultos_nacionales_2011'])):
@@ -138,12 +160,41 @@ for columnaDT in range(inicio_columnas_totalizadas, fin_columnas_totalizadas):
         f2.iloc[filaDT, columnaDT] = categorizado
 
 print('categorizado')
+#Despues de tener el archivo fuente 2 listo, comparamos el archivo fuente 1
+#Queremos saber cuales avistamientos nos sirven (es decir cuales estan en los parques del archivo 2)
+parques = f2['area_protegida'].unique()
+parques = parques.tolist()
+parques.remove('CORALES ROSARIO')
+parques.append('CORALES')
+parques.append('ROSARIO')
+parques.remove('OTUN QUIMBAYA')
+parques.append('OTUN')
+parques.append('QUIMBAYA')
+parques.remove('GUANENTA FONCE')
+parques.append('GUANENTA')
+parques.append('FONCE')
+
+print(parques)
+def buscarPalabras(localidad):
+    return any(parque in localidad for parque in parques)
+
+
+columna_locality = f1.columns.get_loc('locality')
+indices_borrar = []
+for filaDT in range(0, len(f1['locality'])):
+    localidad = f1.iloc[filaDT, columna_locality]
+    if not buscarPalabras(str(localidad)):
+        indices_borrar.append(filaDT)
+
+f1 = f1.drop(f1.index[indices_borrar])
+print('longitud archivo 1: %d'%len(f1))
 
 #Escribir en archivo destino
 #columnas segmentadas unicamente, con sus respectivos totales
 #   (es decir, todo lo que este con informacion mensual)
 #columnas totalizadas anualmente
 #   (es decir, todo lo que no tiene datos de todos los meses sino unicamente de enero)
+f1.to_csv(output1)
 headers = []
 for columnaDT in range(0, f2.columns.get_loc('total_1995')):
     headers.append(f2.columns[columnaDT])
